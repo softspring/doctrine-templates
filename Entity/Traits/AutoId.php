31a2ece1a78f381caf648297b3b7e6fde58f8c6b
Javi H. Gil <javihgil@gmail.com>
2017-12-14 20:47:18 +0100

    Add AutoId trait

---
+++ Entity/Traits/AutoId.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Softspring\DoctrineTemplates\Entity\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Trait AutoId
+ */
+trait AutoId
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
